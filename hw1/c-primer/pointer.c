a9c09e2f1f623d1e7484dffa0057e0f8af9ab968
Peter Griggs <petergriggs17@gmail.com>
2022-08-28 23:31:35 -0400

    [HW1] add macros for printing out size

--- hw1/c-primer/pointer.c
+++ hw1/c-primer/pointer.c
@@ -8,13 +8,14 @@ int main(int argc, char * argv[]) {  // What is the type of argv?
   int i = 5;
   // The & operator here gets the address of i and stores it into pi
   int * pi = &i;
-  // The * operator here dereferences pi and stores the value -- 5 --
+  // The * operator here dereferences pi and stores the value -- 4 --
   // into j.
   int j = *pi;
 
   char c[] = "6.172";
   char * pc = c;  // Valid assignment: c acts like a pointer to c[0] here.
   char d = *pc;
+  printf("char d = %c\n", d);  // What does this print?
   printf("char d = %c\n", d);  // What does this print?
 
   // compound types are read right to left in C.
@@ -27,21 +28,22 @@ int main(int argc, char * argv[]) {  // What is the type of argv?
   char const * pcc2 = c;  // What is the type of pcc2?
 
   // For each of the following, why is the assignment:
-  *pcc = '7';  // invalid?
+  // *pcc = '7';  // invalid? -- invalid because you can't modify a const
   pcc = *pcp;  // valid?
   pcc = argv[0];  // valid?
 
   char * const cp = c;  // cp is a const pointer to char
-  // For each of the following, why is the assignment:
-  cp = *pcp;  // invalid?
-  cp = *argv;  // invalid?
+  // For ehach of the following, why is the assignment:
+  // cp = *pcp;  // invalid?
+  // cp = *argv;  // invalid?
   *cp = '!';  // valid?
 
   const char * const cpc = c;  // cpc is a const pointer to char const
   // For each of the following, why is the assignment:
-  cpc = *pcp;  // invalid?
-  cpc = argv[0];  // invalid?
-  *cpc = '@';  // invalid?
+  // cpc = *pcp;  // invalid?
+  // cpc = argv[0];  // invalid?
+  // *cpc = '@';  // invalid?
 
   return 0;
 }
+
--- hw1/c-primer/sizes.c
+++ hw1/c-primer/sizes.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-#define PRINT_SIZE(typeDesc, typeName) ( printf("size of %s : %zu bytes \n", typeDesc, sizeof(typeName)) )
+#define PRINT_SIZE(typeName) ( printf("size of %s : %zu bytes \n", #typeName, sizeof(typeName)) )
 
 int main() {
   // Please print the sizes of the following types:
@@ -14,10 +14,9 @@ int main() {
 
   // Here's how to show the size of one type. See if you can define a macro
   // to avoid copy pasting this code.
-  printf("size of %s : %zu bytes \n", "int", sizeof(int));
+  /* printf("size of %s : %zu bytes \n", "int", sizeof(int)); */
   // e.g. PRINT_SIZE("int", int);
   //      PRINT_SIZE("short", short);
-  PRINT_SIZE("short", short);
 
   // Alternatively, you can use stringification
   // (https://gcc.gnu.org/onlinedocs/cpp/Stringification.html) so that
@@ -25,21 +24,42 @@ int main() {
   // e.g. PRINT_SIZE(int);
   //      PRINT_SIZE(short);
 
+  PRINT_SIZE(int);
+  PRINT_SIZE(short);
+  PRINT_SIZE(long);
+  PRINT_SIZE(char);
+  PRINT_SIZE(float);
+  PRINT_SIZE(double);
+  PRINT_SIZE(unsigned int);
+  PRINT_SIZE(long long);
+  PRINT_SIZE(uint8_t);
+  PRINT_SIZE(uint16_t);
+  PRINT_SIZE(uint32_t);
+  PRINT_SIZE(uint64_t);
+  PRINT_SIZE(uint_fast8_t);
+  PRINT_SIZE(uint_fast16_t);
+  PRINT_SIZE(uintmax_t);
+  PRINT_SIZE(intmax_t);
+  PRINT_SIZE(__int128);
+
   // Composite types have sizes too.
   typedef struct {
     int id;
     int year;
+    char firstInitial;
   } student;
 
   student you;
   you.id = 12345;
   you.year = 4;
+  you.firstInitial = 'p';
 
 
   // Array declaration. Use your macro to print the size of this.
   int x[5];
 
   // You can just use your macro here instead: PRINT_SIZE("student", you);
+  PRINT_SIZE(x);
   printf("size of %s : %zu bytes \n", "student", sizeof(you));
 
   return 0;
